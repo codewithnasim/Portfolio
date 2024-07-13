d443066d939623d86712d06263ade31d23745b35
NASIM ISLAM <126854586+nasim7047@users.noreply.github.com>
2024-07-13 20:00:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio",
-  plugins: [react()],
-})
+  // Specify the entry point
+  build: {
+    rollupOptions: {
+      input: 'main.jsx'
+    }
+  }
+});
