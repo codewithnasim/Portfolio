dbfa79463418718d2edae46af5a791ed3b9c58de
NASIM ISLAM <126854586+codewithnasim@users.noreply.github.com>
2024-07-19 16:06:52 +0530

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,126 +1,67 @@
-"use client";
-"use client";
-import { Link } from "react-router-dom";
 import React from "react";
-import con from "../assets/contact.avif";
+import { FiDownload } from "react-icons/fi";
+import cv from "../assets/Resume.pdf";
+// import jsPDF from "jspdf";
 
-export default function Contact() {
-  const Submit = () => {
-    document.getElementById("btn").innerHTML = "Request submitted succesfully";
-  };
+import "../css/style.css";
+import bgImage from "../assets/bg.jpg";
+import prImage from "../assets/nas.jpg";
+import { Link } from "react-router-dom";
+
+const Home = () => {
   return (
     <>
-      <div className="mx-auto max-w-7xl px-4">
-        {/* Hero Map */}
-
-        <div className="mx-auto max-w-7xl py-12 md:py-24">
-          <div className="grid items-center justify-items-center gap-x-4 gap-y-10 lg:grid-cols-2">
-            {/* contact from */}
-            <div className="flex items-center justify-center">
-              <div className="px-2 md:px-12">
-                <p className="text-2xl font-bold  md:text-4xl text-center">
-                  Contact us{" "}
-                </p>
-                <p className="mt-4 text-lg text-center">
-                  <a href="mailto:srs.nasim@gmail.com">Email me</a>
-                </p>
-                <p className="text-center p-2 text-xl font-bold">or</p>
-                <form action="/submit" className="mt-8 space-y-4">
-                  <div className="grid w-full gap-y-4 md:gap-x-4 lg:grid-cols-2">
-                    <div className="grid w-full  items-center gap-1.5">
-                      <label
-                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                        htmlFor="first_name">
-                        First Name
-                      </label>
-                      <input
-                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
-                        type="text"
-                        id="first_name"
-                        placeholder="First Name"
-                      />
-                    </div>
-                    <div className="grid w-full  items-center gap-1.5">
-                      <label
-                        className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                        htmlFor="last_name">
-                        Last Name
-                      </label>
-                      <input
-                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
-                        type="text"
-                        id="last_name"
-                        placeholder="Last Name"
-                      />
-                    </div>
-                  </div>
-                  <div className="grid w-full  items-center gap-1.5">
-                    <label
-                      className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                      htmlFor="email">
-                      Email
-                    </label>
-                    <input
-                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
-                      type="text"
-                      id="email"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div className="grid w-full  items-center gap-1.5">
-                    <label
-                      className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                      htmlFor="phone_number">
-                      Phone number
-                    </label>
-                    <input
-                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
-                      type="tel"
-                      id="phone_number"
-                      placeholder="Phone number"
-                    />
-                  </div>
-                  <div className="grid w-full  items-center gap-1.5">
-                    <label
-                      className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                      htmlFor="message">
-                      Message
-                    </label>
-                    <textarea
-                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
-                      id="message"
-                      placeholder="Leave us a message"
-                      cols={3}
-                    />
-                  </div>
-                  <Link to={"/submit"}>
-                    {" "}
-                    <button
-                      type="button"
-                      id="btn"
-                      className="w-full rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                      onClick={() => {
-                        document.getElementById("contact").innerText =
-                          "Submitted Successful, You will get response after some time";
-                        document.getElementById("contact").style.textAlign =
-                          "center";
-                        document.getElementById("contact").style.fontSize =
-                          "30px";
-                      }}>
-                      Send Message
-                    </button>
-                  </Link>
-                </form>
+      <div className="relative w-full ">
+        <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
+          <div className="flex flex-col justify-center px-4 py-12 md:py-16 lg:col-span-7 lg:gap-x-6 lg:px-6 lg:py-24 xl:col-span-6">
+            <h1 className="mt-8 text-6xl tracking-tight md:text-6xl lg:text-6xl font-semibold font-serif">
+              Hi,
+              <br />I am <h1 className="text-blue-600 inline-block">Nasim</h1>
+            </h1>
+            <p className="mt-8  text-5xl text-rose-900 animate-bounce ">
+              Full Stack Web Developer
+            </p>
+            <form action="" className="mt-8 flex items-start space-x-2">
+              <div>
+                <Link to={cv}>
+                  <button
+                    type="button"
+                    className="rounded-md bg-black px-3 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">
+                    Download CV
+                  </button>
+                </Link>
               </div>
-            </div>
+              <div>
+                <Link to={"/contact"}>
+                  <button
+                    type="button"
+                    className="rounded-md bg-black px-3 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">
+                    Contact
+                  </button>
+                </Link>
+              </div>
+              <div>
+                <Link to={"/project"}>
+                  <button
+                    type="button"
+                    className="rounded-md bg-black px-3 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">
+                    Project
+                  </button>
+                </Link>
+              </div>
+            </form>
+            <p className="mt-20"></p>
+          </div>
+          <div className="relative lg:col-span-5 lg:-mr-8 xl:col-span-6">
             <img
-              alt="Contact us"
-              className="hidden max-h-md max-w-md rounded-lg object-cover lg:block"
-              src={con}
+              className=" object-cover  p-3 rounded-3xl mt-10 h-3/5"
+              src={prImage}
+              alt=""
             />
           </div>
         </div>
       </div>
     </>
   );
-}
+};
+export default Home;
